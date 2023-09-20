420140515bf4d25d55f85c776211f3bbd2847f13
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-09-19 23:32:23 -0400

    fix
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/install/test_on_kaggle.py
+++ source/install/test_on_kaggle.py
@@ -8,7 +8,13 @@ def test_on_kaggle():
         ["git", "clone", "https://github.com/njzjz/deepmd-kit", "-b", "test_on_kaggle"]
     )
     os.chdir("deepmd-kit")
-    subprocess.check_output(["./source/install/test_cc_local.sh"])
+    subprocess.check_output(
+        ["bash", "./source/install/test_cc_local.sh"],
+        env={
+            "DP_VARIANT": "cuda",
+            **os.environ,
+        },
+    )
 
 
 if __name__ == "__main__":
