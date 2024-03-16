b00cf7534c408e488769dfa4da9a1153beecf3f7
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-03-15 23:27:48 -0400

    finalize
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- source/lmp/tests/mpi_helper.py
+++ source/lmp/tests/mpi_helper.py
@@ -2,12 +2,14 @@
 try:
     from mpi4py import (
         MPI,
+        rc,
     )
 except ModuleNotFoundError:
     MPI = None
     RANK = 0
 else:
     RANK = MPI.COMM_WORLD.rank
+    rc.finalize = True
 
 __all__ = [
     "RANK",
