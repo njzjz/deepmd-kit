c1a42e40f3d0f17d2da06854c4c00b3665728eac
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-05-23 17:34:35 -0400

    ci: run cmake before setup codeql

--- source/install/build_cc.sh
+++ source/install/build_cc.sh
@@ -20,15 +20,19 @@ NPROC=$(nproc --all)
 BUILD_TMP_DIR=${SCRIPT_PATH}/../build
 mkdir -p ${BUILD_TMP_DIR}
 cd ${BUILD_TMP_DIR}
-cmake -D ENABLE_TENSORFLOW=ON \
-	-D ENABLE_PYTORCH=ON \
-	-D CMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-	-D USE_TF_PYTHON_LIBS=TRUE \
-	${CUDA_ARGS} \
-	-D LAMMPS_VERSION=stable_2Aug2023_update3 \
-	..
-cmake --build . -j${NPROC}
-cmake --install .
+if [ "$DP_NO_CMAKE" = "" ] then
+	cmake -D ENABLE_TENSORFLOW=ON \
+		-D ENABLE_PYTORCH=ON \
+		-D CMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+		-D USE_TF_PYTHON_LIBS=TRUE \
+		${CUDA_ARGS} \
+		-D LAMMPS_VERSION=stable_2Aug2023_update3 \
+		..
+fi
+if [ "$DP_NO_BUILD" = "" ]; then
+	cmake --build . -j${NPROC}
+	cmake --install .
+fi
 
 #------------------
 echo "Congratulations! DeePMD-kit has been installed at ${INSTALL_PREFIX}"
