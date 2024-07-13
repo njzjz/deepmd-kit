dd6629a630796cb10d8089bf6987bc51c98e232f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-07-13 02:06:00 -0400

    fix typo

--- source/op/pt/mace.cc
+++ source/op/pt/mace.cc
@@ -44,7 +44,7 @@ torch::Tensor mace_edge_index(const torch::Tensor& nlist_tensor,
     }
   }
   // convert to tensor
-  int64_t edge_size = edge_index.size();
+  int64_t edge_size = edge_index.size() / 2;
   torch::Tensor edge_index_tensor =
       torch::from_blob(edge_index.data(), {edge_size, 2}, torch::kInt64);
   // to nlist_tensor.device
